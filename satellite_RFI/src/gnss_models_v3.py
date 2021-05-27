dd6bb4c85e664096747c4c1ad6b2785f76fba425
BrandonE314 <engelbrechtbn@gmail.com>
2021-05-27 11:00:00 +0200

    Added the correct import paths

--- satellite_RFI/src/gnss_models_v3.py
+++ satellite_RFI/src/gnss_models_v3.py
@@ -56,7 +56,7 @@ def gnss_satellites(name_gnss, frequency_gnss):
         return -1
 
     
-    data = pd.read_csv('Satellite_Catalogue/table3B_satellite_v3.csv', header=0, engine='python')   # Excel data file with all the GNSS and models Table 2
+    data = pd.read_csv('../Notebooks/Satellite_simulations/Satellite_Catalogue/table3B_satellite_v3.csv', header=0, engine='python')   # Excel data file with all the GNSS and models Table 2
 
     # Re-ordering by frequency
 #         data = data.sort_values(by = 'Frequency[MHz]')
