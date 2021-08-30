f1cd456f5dbd351c38027e39d90c2b393f019101
BrandonE314 <engelbrechtbn@gmail.com>
2021-08-30 12:52:38 +0200

    Update version mumber

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='satellite_RFI',
-    version='0.0.6',
+    version='0.0.9',
     description='This package contains GNSS satellite simulation for HI IM',
     author='Brandon Engelbrecht',
     author_email='engelbrechtbn@gmail.com',
