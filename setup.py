374282e0b30601784984014f48dd7b2d8c3e7bfc
BrandonE314 <engelbrechtbn@gmail.com>
2021-09-02 09:36:07 +0200

    Change version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='satellite_RFI',
-    version='0.0.9',
+    version='0.0.9.2.2',
     description='This package contains GNSS satellite simulation for HI IM',
     author='Brandon Engelbrecht',
     author_email='engelbrechtbn@gmail.com',
