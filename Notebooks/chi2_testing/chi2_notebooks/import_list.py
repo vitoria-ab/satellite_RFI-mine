a0c4174b06a66d3006caceadd2cad336f0db624f
BrandonE314 <engelbrechtbn@gmail.com>
2022-04-19 10:42:27 +0200

    Added various notebooks and paramter files for the chi2 testing

--- Notebooks/chi2_testing/chi2_notebooks/import_list.py
+++ Notebooks/chi2_testing/chi2_notebooks/import_list.py
@@ -3,22 +3,26 @@ A list of imports for the chi2 notebooks.
 Note; should be used everywhere
 '''
 
-
+# From satellite_rfi modules
 from satellite_RFI.src.satellite_sims import satellite_sim as ss
 import satellite_RFI.src.tools as tools
 
+# Parameter list
 import param as param
 
 import time
 import pickle
-import astropy.units as u
-from datetime import datetime
 import tqdm
 import os
-
+from datetime import datetime
 
 import scipy as sp
 import numpy as np
 import pandas as pd
+import astropy.units as u
 import scipy.optimize as opt
 import matplotlib.pyplot as plt
+
+# Parellization
+import multiprocessing as mp
+from threading import Thread
--- Notebooks/chi2_testing/chi2_notebooks/param.py
+++ Notebooks/chi2_testing/chi2_notebooks/param.py
@@ -5,34 +5,42 @@ Note; this should be untilized everywhere
 
 # File name [The unix time of observation]
 file_name='1551055211'
+file_name_ext=''
 
 # The yyyy MM DD hh mm ss of the observation ['2021 9 30 20 06 36']
 observation_time=None
 
 # KATDAL path
-telescope_info_path='/idia/projects/hi_im/brandon/'+file_name+'/'+file_name+'_katdal_info.p'
+telescope_info_path='/idia/projects/hi_im/satellite_rfi/Observations/'+file_name+file_name_ext+'/'+file_name+'_katdal_info.p'
 
 # Frequency start and end
 fs, fe=1000,1500
 
 # Path to save the simulation file
-save_data='../data_test/'+file_name+'/pickle_info/'
+sat_sim_save_data='/idia/projects/hi_im/satellite_rfi/Results/Data/'+file_name+file_name_ext+'/pickle_info/'
 
 # MeerKAT observation maps and results
-meerkat_data='../../../../Observation_results/Untangle/'+file_name+'/'
+meerkat_data='/idia/projects/hi_im/satellite_rfi/Observations/Untangle/'+file_name+file_name_ext+'/'
 
 # Nearby constellation times
+    # Degree value
 deg='2'    #[Can be '#_fill' or '#']
-nearby_constellation_path = '../nearby_satellite_mask/nearby_satellite_close_angle_'+deg+'.p'
+    # Full path
+nearby_constellation_fpath = '/idia/projects/hi_im/satellite_rfi/Observations/'+file_name+'/nearby_satellite_mask/nearby_satellite_close_angle_'+deg+'.p'
+    # Suffix needed
+nearby_constellation_path = '/idia/projects/hi_im/satellite_rfi/Observations/'+file_name+'/nearby_satellite_mask/'
 
 # Work folder to save results
-work_folder='2022_03_20'
+work_folder='parallel_2022_03_20'
+
+# Path to save chi_2 best-fit values
+chi2_save_data='/idia/projects/hi_im/satellite_rfi/Results/Data/'+file_name+file_name_ext+'/data_info/'+work_folder+'/'
 
 # Satellite catalogue path 
 satellite_catalogue='../../Satellite_simulations/Satellite_Catalogue/table3B_satellite_v3-1_reduced_2.csv'
 
 # Plot location
-save_plots='data_test_plots/'+file_name+'/'+work_folder+'/'
+save_plots='/idia/projects/hi_im/satellite_rfi/Results/Plots/'+file_name+file_name_ext+'/'+work_folder+'/'
 
 # Beam model
 beam_model='emss_beam_r'
